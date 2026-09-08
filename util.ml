d3f6251fabfc297c39e65448cb03b9ce934fea63
Ion <i.chirica@campus.fct.unl.pt>
2026-09-08 14:46:55 +0100

    missing util.ml

---
+++ util.ml
@@ -0,0 +1,28 @@
+open Ast
+
+let rec subst_expr su expr =
+  match expr with
+  | Const c -> Const c
+  | Variable x ->
+      (match VarMap.find_opt x su with
+       | Some e -> e
+       | None -> Variable x)
+  | Unary (op, e) ->
+      Unary (op, subst_expr su e)
+  | Binary (op, e1, e2) ->
+      Binary (op, subst_expr su e1, subst_expr su e2)
+
+let rec subst_prop su prop =
+  match prop with
+  | False -> False
+  | True -> True
+  | Equality (e1, e2) ->
+      Equality (subst_expr su e1, subst_expr su e2)
+  | UnaryOp (op, p) ->
+      UnaryOp (op, subst_prop su p)
+  | NaryOp (op, ps) ->
+      NaryOp (op, List.map (subst_prop su) ps)
+  | BinaryOp (op, p1, p2) ->
+      BinaryOp (op, subst_prop su p1, subst_prop su p2)
+  | Quantified (q, vars, p) ->
+      Quantified (q, vars, subst_prop su p)
